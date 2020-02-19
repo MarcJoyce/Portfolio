262b6a329779e415aea6f3364d3e9eb210974125
Marc Joyce <60116589+MarcJoyce@users.noreply.github.com>
2020-02-19 10:15:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-$("#toggle").click(function(){
-   $(this).toggleClass('on');
-   $("#resize").toggleClass("active");
-});
-
-$("#resize ul li a").click(function(){
-   $(this).toggleClass('on');
-   $("#resize").toggleClass("active");
-});
-
-$(".close-btn").click(function(){
-   $(this).toggleClass('on');
-   $("#resize").toggleClass("active");
-});
-
+$("#toggle").click(function() {
+            $(this).toggleClass('on');
+            $("#resize").toggleClass("active");
+      });
+      $("#resize ul li a").click(function() {
+            $(this).toggleClass('on');
+            $("#resize").toggleClass("active");
+      });
+      $(".close-btn").click(function() {
+            $(this).toggleClass('on');
+            $("#resize").toggleClass("active");
+      });
