b8b1b7c49c080abbf7e43e1f813272f3372a2a49
Marc Joyce <marcjoyce90@gmail.com>
2021-03-29 22:06:20 +0100

    create hero section create work section create experience section create nav bar

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggle() {
-  var toggle = document.getElementById("toggle");
-  var nav = document.getElementById("nav-bar");
-  toggle.classList.toggle("active");
-  nav.classList.toggle("active");
-}
