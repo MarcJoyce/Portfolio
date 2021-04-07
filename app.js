a3840320cafeb8aa5d0f64e9f8560d423755bd30
Marc Joyce <marcjoyce90@gmail.com>
2021-04-07 21:29:31 +0100

    add footer

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ const toggleContactMenu = () => {
   const contact = document.querySelector("#contact");
   contact.classList.toggle("active");
 }
+
+const setCopyrightYear = () => {
+  const year = document.querySelector("#copyright__year");
+  const currentYear = new Date().getFullYear();
+  year.innerHTML = currentYear;
+}
+
+setCopyrightYear();
