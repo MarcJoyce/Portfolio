1947b0272e5c8a2d2dd784bf010cab7697f29a14
Marc Joyce <Marc.Joyce@shelbyfinance.com>
2022-03-15 12:22:41 +0000

    [Add Google Analytics] Add Google Analytics for page view

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
-import React from 'react';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
-
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import Home from './components/Home/Home.js';
 
 function App() {
+
+  const TRACKING_ID = 'UA-222888625-2';
+
+  ReactGA.initialize(TRACKING_ID);
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <Router>
-      <Home />
+        <Route path="/" exact component={Home} />
     </Router>
   )
 }
